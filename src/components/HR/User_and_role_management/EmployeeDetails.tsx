5d55fe1dbded8222250bd033631458e23cb73e53
seunosinowo <oluwaseunpaul98@gmail.com>
2025-05-10 17:49:20 +0100

    Resolve Typescript errors

--- src/components/HR/User_and_role_management/EmployeeDetails.tsx
+++ src/components/HR/User_and_role_management/EmployeeDetails.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { UserIcon, XMarkIcon, PlusIcon } from '@heroicons/react/24/outline';
+import { XMarkIcon, PlusIcon } from '@heroicons/react/24/outline';
 import { supabase } from '../../../lib/supabase';
 import { uploadImage, getDefaultAvatarUrl, checkBucketExists } from '../../../utils/imageUpload';
 import { useAuth } from '../../../context/AuthContext';
