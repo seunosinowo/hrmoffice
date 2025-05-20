0c5677653b79e2ccb1560ec6cfca655fe2b3919c
seunosinowo <oluwaseunpaul98@gmail.com>
2025-05-20 13:10:05 +0100

    Added new message

--- src/pages/HR/Assessment_management/AssessorAssessment.tsx
+++ src/pages/HR/Assessment_management/AssessorAssessment.tsx
@@ -425,7 +425,7 @@ export default function AssessorAssessment() {
                         <svg className="h-3.5 w-3.5 text-blue-400 mr-1 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor">
                           <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
                         </svg>
-                        You have reviewed this assessment. You can edit your ratings or export to PDF.
+                        This assessment has been reviewed by an assessor. You can view the details or export to PDF.
                       </p>
                     </div>
 
